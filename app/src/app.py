8ef82dbcd45cdb4b7c43385aae53d4f491ca2c33
Rodrigo Tomio Kumabe <rodrigo.kumabe@gmail.com>
2024-09-08 18:19:40 -0300

    feat: include tests

--- app/src/app.py
+++ app/src/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, url_for
-from markupsafe import escape
-from flask_restful import Resource, Api, reqparse, request
-from prometheus_client import Gauge, Counter, make_wsgi_app
+"""This is the main file that we use to start ervil app."""
+
+
+from flask import Flask #, url_for
+#from markupsafe import escape
+from flask_restful import Api, request #, Resource, reqparse
+from prometheus_client import Counter, make_wsgi_app #, Gauge
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 
@@ -17,21 +20,25 @@ app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
 login_com_sucesso = Counter("login_com_sucesso", "Quantidade de login com sucesso!")
 login_com_falha = Counter("login_com_falha", "Quantidade de login com falha!")
 
+
 @app.route('/')
 def index():
+    """Application Route for index."""
     return 'Hello, World!'
-
 
 @app.route('/login', methods=["POST"])
 def login():
+    """Application Route for login."""
     username = request.json['username']
     password = request.json['password']
+    retorno = None
     if username == "admin" and password == "password":
         login_com_sucesso.inc()
-        return f"Usuário {username} logado com sucesso", 201
+        retorno = f"Usuário {username} logado com sucesso", 201
     else:
         login_com_falha.inc()
-        return f"Ocorreu um problema ao fazer login!", 500
+        retorno = "Ocorreu um problema ao fazer login!", 401
+    return retorno
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
